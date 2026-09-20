59c78591eb69e74c603048417cb6cf64212fbbcb
Prarinam Deria <prarinamderia2000@gmail.com>
2026-09-20 21:33:49 +0530

    graph building done
    Signed-off-by: Prarinam Deria <prarinamderia2000@gmail.com>

--- graph.py
+++ graph.py
@@ -49,3 +49,35 @@ def route_after_agent(current_agent: str):
                 return next_agent
 
     return route
+
+
+def build_graph() -> StateGraph:
+    graph = StateGraph(TravelState)
+
+    graph.add_node("supervisor", supervisor_agent)
+    graph.add_node("flight_agent", flight_agent)
+    graph.add_node("hotel_agent", hotel_agent)
+    graph.add_node("weather_agent", weather_agent)
+    graph.add_node("budget_agent", budget_agent)
+    graph.add_node("itinerary_agent", itinerary_agent)
+    graph.add_node("human_approval", human_approval_agent)
+    graph.add_node("final_response", final_response_agent)
+
+    graph.add_edge(START, "supervisor")
+    graph.add_conditional_edge("supervisor", route_from_supervisor, ROUTE_MAP)
+    graph.add_conditional_edges("flight_agent", route_after_agent("flight_agent"), ROUTE_MAP)
+    graph.add_conditional_edges("hotel_agent", route_after_agent("hotel_agent"), ROUTE_MAP)
+    graph.add_conditional_edges("weather_agent", route_after_agent("weather_agent"), ROUTE_MAP)
+    graph.add_conditional_edges("budget_agent", route_after_agent("budget_agent"), ROUTE_MAP)
+    graph.add_edge("itinerary_agent", "human_approval")
+    graph.add_edge("human_approval", "final_response")
+    graph.add_edge("final_response", END)
+
+    if DATABASE_URL:
+        conn = psycopg.connect(DATABASE_URL)
+        checkpointer = PostgresSaver(conn)
+        checkpointer.setup()
+        return graph.compile(checkpointer=checkpointer)
+
+    return graph.compile()
+
